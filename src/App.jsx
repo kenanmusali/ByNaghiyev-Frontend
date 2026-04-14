f7d3ae1deb47d02d99cc3f93f7b2d0329cdfe127
kenanmusali <textme0306@gmail.com>
2026-04-14 10:58:22 +0400

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,58 @@
+// // App.js
+// import About from "./components/about";
+// import Blogs from "./components/blogs";
+// import Category from "./components/category";
+// import Footer from "./components/footer";
+// import Header from "./components/header";
+// import Navbar from "./components/navbar";
+// import Products from "./components/products";
+// import ZoomWrapper from "./ZoomWrapper";
+// import EditAdmin from "./admin";
+
+// const App = () => {
+//   return (
+//     < div className="AdminOn">
+//         <Navbar />
+//         {/* <EditAdmin/> */}
+
+//         <Header />
+//         {/* <EditAdmin/> */}
+
+//         <ZoomWrapper>
+//         <About />
+//         </ZoomWrapper>
+//         {/* <EditAdmin/> */}
+
+
+        
+       
+//         <Category />
+//         {/* <EditAdmin/> */}
+      
+
+//         <ZoomWrapper>
+//         <Products />
+//         </ZoomWrapper>
+//         {/* <EditAdmin/> */}
+
+//         <ZoomWrapper>
+//         <Blogs />
+//         </ZoomWrapper>
+//         {/* <EditAdmin/> */}
+
+      
+//         <Footer />
+//         {/* <EditAdmin/> */}
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+
 // App.js
+import { useEffect, useState } from "react";
 import About from "./components/about";
 import Blogs from "./components/blogs";
 import Category from "./components/category";
@@ -10,39 +64,79 @@ import ZoomWrapper from "./ZoomWrapper";
 import EditAdmin from "./admin";
 
 const App = () => {
+  const [isZoomRange, setIsZoomRange] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+      setIsZoomRange(width <= 1400 && width >= 1024);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    < div className="AdminOn">
-        <Navbar />
-        {/* <EditAdmin/> */}
+    <div className="AdminOn">
+      {isZoomRange ? (
+        <>
+          <ZoomWrapper>
+            <Navbar />
+          </ZoomWrapper>
 
-        <Header />
-        {/* <EditAdmin/> */}
+          <Header />
 
-        <ZoomWrapper>
-        <About />
-        </ZoomWrapper>
-        {/* <EditAdmin/> */}
+          <ZoomWrapper>
+            <ZoomWrapper>
+              <About />
+            </ZoomWrapper>
+          </ZoomWrapper>
 
+          <ZoomWrapper>
+            <Category />
+          </ZoomWrapper>
 
-        
-       
-        <Category />
-        {/* <EditAdmin/> */}
-      
+          <ZoomWrapper>
+            <ZoomWrapper>
+              <Products />
+            </ZoomWrapper>
+          </ZoomWrapper>
 
-        <ZoomWrapper>
-        <Products />
-        </ZoomWrapper>
-        {/* <EditAdmin/> */}
+          <ZoomWrapper>
+            <ZoomWrapper>
+              <Blogs />
+            </ZoomWrapper>
+          </ZoomWrapper>
 
-        <ZoomWrapper>
-        <Blogs />
-        </ZoomWrapper>
-        {/* <EditAdmin/> */}
+          <ZoomWrapper>
+            <Footer />
+          </ZoomWrapper>
+        </>
+      ) : (
+        <>
+          <Navbar />
 
-      
-        <Footer />
-        {/* <EditAdmin/> */}
+          <Header />
+
+          <ZoomWrapper>
+            <About />
+          </ZoomWrapper>
+
+          <Category />
+
+          <ZoomWrapper>
+            <Products />
+          </ZoomWrapper>
+
+          <ZoomWrapper>
+            <Blogs />
+          </ZoomWrapper>
+
+          <Footer />
+        </>
+      )}
+      {/* <EditAdmin/> */}
     </div>
   );
 };
